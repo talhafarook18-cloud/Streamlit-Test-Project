74667bdf70c5d86385f5bc7d81b9a25e3f282305
talhafarook18-cloud <talhafarook18@gmail.com>
2026-01-13 21:24:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from google import genai
+from google.genai import types
 import streamlit as st
 
 client = genai.Client (api_key='AIzaSyA3uvDeUuRiY-E4Zcl2JOMSWOSxJONcwfc')
 
-st.title("Finance Advisor")
+st.title("Finance Advisor  ")
 st.write("This app demonstrates a conversational agent.")
 
 user_input = st.text_input("How can I help you.")
 if st.button("Submit"):
     with st.spinner("Agent is thinking...."):
         response = client.models.generate_content(
-             model='gemini-2.5-flash', contents=user_input
+            model='gemini-2.5-flash', contents=user_input,
+            config=types.GenerateContentConfig(
+    system_instruction="Your name is IBBI and you are a finance advisor who only answere relevant questions."),
         )
     st.write(response.text)
-
-
-
